934039bfba13f39d607fc1e5ebabd6c4f518bd34
AbagnaleFlorencia <abagnaleflorenciag@gmail.com>
2022-01-12 20:21:59 -0300

    Cambio-conHeadearPHP

--- contacto.php
+++ contacto.php
@@ -1,3 +1,6 @@
+<?php 
+    $pg="contacto";
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -14,35 +17,7 @@
 
 </head>
 <body id="contacto">
-    <header class="container px-0">
-        <nav class="navbar navbar-expand-md ">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav mb-2 mb-mt-2">
-                        <li class="nav-item  px-3"> 
-                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mí</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link px-4 py-1" href="proyectos.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link active px-4 py-1" href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="d-inline">
-                    <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php include_once "header.php"; ?>
     
     <main class="container">
         <div class="row">
---
+++ header.php
@@ -0,0 +1,30 @@
+<header class="container px-0">
+        <nav class="navbar navbar-expand-md ">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mb-2 mb-mt-2">
+                        <li class="nav-item  px-3">
+                            <a class="nav-link px-4 py-1  <?php echo ($pg =="inicio") ? "active" : "" ?>" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link px-4 py-1  <?php echo ($pg =="sobre-mi") ? "active" : "" ?>" href="sobre-mi.php">Sobre mí</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link px-4 py-1  <?php echo ($pg =="proyectos") ? "active" : "" ?>" href="proyectos.php">Proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link px-4 py-1 <?php echo ($pg =="contacto") ? "active" : "" ?>" href="contacto.php">Contacto</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="d-inline">
+                    <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
+                </div>
+            </div>
+        </nav>
+    </header>
